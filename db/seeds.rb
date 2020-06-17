b7819e496a7390d6ca38dc388ef50cf246a485fd
James Yeates <jimibue@gmail.com>
2020-06-17 10:24:49 -0600

    setup planet and solar_sytems models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-100.times do
+Product.destroy_all
+10.times do
   Product.create(
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.sentence,
@@ -8,3 +9,32 @@
 end
 
 puts "100 Products Seeded"
+
+# create_table "planets", force: :cascade do |t|
+#   t.string "name"
+#   t.boolean "inhabited"
+#   t.integer "size"
+#   t.bigint "solar_system_id", null: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["solar_system_id"], name: "index_planets_on_solar_system_id"
+# end
+
+# create_table "solar_systems", force: :cascade do |t|
+#   t.string "name"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
+
+# Faker::Space.planet
+SolarSystem.destroy_all
+Planet.destroy_all
+10.times do |i|
+  s = SolarSystem.create(name: Faker::Space.galaxy)
+  5.times do |j|
+    s.planets.create(name: Faker::Space.meteorite, inhabited: false, size: rand(1000...100000))
+  end
+end
+
+puts "SolarSystem seed size is #{SolarSystem.all.size}"
+puts "planets seed size is #{Planet.all.size}"
